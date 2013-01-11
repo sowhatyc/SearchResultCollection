f2de6954ab38241fa2fa1e75ee218cceeeaae831
YangC <YangC@YangC-PC>
2013-01-11 16:01:24 +0800

    first test

---
+++ src/gittest/GitTest.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gittest;
+
+/**
+ *
+ * @author YangC
+ */
+public class GitTest {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("It's just a test for git!");
+    }
+}
